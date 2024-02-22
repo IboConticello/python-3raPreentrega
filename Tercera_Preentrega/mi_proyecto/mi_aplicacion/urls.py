6af1807e8687e7e56e8c0ca70b2773dbbaee5dc2
Conticello <ibo.j.conticello@outlook.com>
2024-02-22 01:08:30 -0300

    upload new project

--- Tercera_Preentrega/mi_proyecto/mi_aplicacion/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('formulario/', views.formulario_insertar, name='formulario_insertar'),
-    path('buscar/', views.buscar, name='buscar'),
-    path('resultado/', views.resultado_busqueda, name='resultado_busqueda'),
-]
--- Tercera_Preentrega/mi_proyecto/mi_proyecto/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from mi_aplicacion import views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.inicio, name='inicio'),
-    path('formulario/', include('mi_aplicacion.urls')),
-]
